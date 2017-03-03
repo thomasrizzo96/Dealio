93db3381e1d0b13f7156ded9058e6614276c8155
aaronmorgenegg <aaronmorgenegg@gmail.com>
2017-03-02 18:22:20 -0700

    added to urls

--- dealioApp/urls.py
+++ dealioApp/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^restaurants$', views.restaurants, name='restaurants'),
     url(r'^promotions/(?P<restaurant_id>\d+)/$', views.promotions, name='promotions'),#this uses a named group for regex
-    url(r'^ownerLogin$', views.ownerLogin, name = 'ownerLogin'),
+    url(r'^ownerLogin$', views.ownerLogin, name ='ownerLogin'),
 
 ]
