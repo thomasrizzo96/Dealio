fed503e1fde0d210ea7a31f092715f5a678c38e1
Thomas Rizzo <thomas.rizzo96@gmail.com>
2017-03-20 11:40:01 -0600

    added restaurant update class, url, and button(on promotions.html)

--- dealioApp/urls.py
+++ dealioApp/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^restaurants$', views.restaurants, name='restaurants'),
     url(r'^promotions/(?P<restaurant_id>\d+)/$', views.promotions, name='promotions'),#this uses a named group for regex
     url(r'^restaurant/add/$', views.RestaurantCreate.as_view(), name='restaurant-add'),
+    url(r'^restaurant/(?P<pk>\d+)/$', views.RestaurantUpdate.as_view(), name='restaurant-update'),
     url(r'^ownerLogin$', views.ownerLogin, name ='ownerLogin'),
     url(r'^placefinder$', views.placefinder, name='placefinder'),
 ]
